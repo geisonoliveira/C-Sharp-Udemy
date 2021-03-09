c581e6c37a33966174265eab6ce2fae768a9b843
geisonoliveira <gs_gaara@hotmail.com>
2021-03-09 19:56:00 -0300

    Nova Aula

---
+++ 11- Estruturas Condicionais/EstruturasCondicionais/EstruturasCondicionais/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EstruturasCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, digite um numero inteiro: ");
+            
+            int numero = int.Parse(Console.ReadLine());
+            
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O numero digitado é PAR");
+            }
+
+            else
+            {
+                Console.WriteLine("O numero digitado é IMPAR");
+            }
+
+            Console.WriteLine("Qual a hora atual?: ");
+
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa Tarde!");
+            }
+
+            else
+            {
+                Console.WriteLine("Boa Noite!");
+            }
+
+        }
+    }
+}
