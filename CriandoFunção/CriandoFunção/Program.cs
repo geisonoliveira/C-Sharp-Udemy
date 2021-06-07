9e4760e20c2c49574f1268ad76872488c9857da2
Geison  Oliveira <gs_gaara@hotmail.com>
2021-06-07 20:38:19 -0300

    Voltando aos estudos

---
+++ CriandoFunção/CriandoFunção/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace CriandoFunção
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 valores separados por espaço");
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            double numero1 = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+            double numero2 = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+            double numero3 = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+            double resultado = Maior(numero1, numero2, numero3);
+
+            Console.WriteLine(resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static double Maior(double a, double b, double c)
+        {
+            double m;
+            
+            if (a > b && a > c) {
+                m = a;
+            }
+
+            else if (b > c)
+            {
+                m = b;
+            }
+
+            else
+            {
+                m = c;
+            }
+
+            return m;
+        }
+    }
+}
---
+++ RevisãoWhile/RevisãoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace RevisãoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+
+            double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(Math.Sqrt(numero).ToString("F3", CultureInfo.InvariantCulture));
+
+            while (numero > 0)
+            {
+                Console.WriteLine("Digite outro numero: ");
+                numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine(Math.Sqrt(numero).ToString("F3", CultureInfo.InvariantCulture));
+            }
+
+            Console.WriteLine("Numero negativo!");
+        }
+    }
+}
