b7d8b989829eae3a960caa6feb2f72a286d5d04e
Geison  Oliveira <gs_gaara@hotmail.com>
2021-06-08 06:39:37 -0300

    Exercicio 3

---
+++ Exercícios Propostos 3 - While/ExercicioProposto3/ExercicioProposto3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ExercicioProposto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            int entrada = 0;
+
+            while (entrada != 4)
+            {
+                Console.WriteLine("Entre com a opção desejada: ");
+                Console.WriteLine("1 - Alcool");
+                Console.WriteLine("2 - Gasolina");
+                Console.WriteLine("3 - Diesel");
+                Console.WriteLine("4 - Sair");
+
+                entrada = int.Parse(Console.ReadLine());
+
+                if (entrada == 1)
+                {
+                    alcool++;
+                }
+
+                else if (entrada == 2)
+                {
+                    gasolina++;
+                }
+
+                else if (entrada == 3)
+                {
+                    diesel++;
+                }
+                    
+                else
+                {
+                    
+                }
+            }
+
+            Console.WriteLine("Muito Obrigado!");
+            Console.WriteLine($"Alcool: {alcool}");
+            Console.WriteLine($"Gasolina: {gasolina}");
+            Console.WriteLine($"Diesel: {diesel}");
+
+        }
+    }
+}
