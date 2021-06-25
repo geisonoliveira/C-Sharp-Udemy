8e9f5cd691b3d1b30c335cbea15e8367485a4fea
Geison  Oliveira <gs_gaara@hotmail.com>
2021-06-25 19:16:18 -0300

    Novas Aulas classe

---
+++ 15 - SEÇÃO 4 - Classes, atributos, metodos/2 - Criando uma Classe com 3 atributos/CriandoPrimeiraClasse/CriandoPrimeiraClasse/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com as medidas do triangulo X");
+
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y");
+
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine($"A area de X {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"A area de Y {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior area = X");
+            }
+
+            else
+            {
+                Console.WriteLine("Maior area = Y");
+            }
+
+        }
+    }
+}
---
+++ 15 - SEÇÃO 4 - Classes, atributos, metodos/2 - Criando uma Classe com 3 atributos/CriandoPrimeiraClasse/CriandoPrimeiraClasse/Triangulo.cs
@@ -0,0 +1,10 @@
+﻿namespace CriandoPrimeiraClasse
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
