7320dbb92272b9c90bdde063ce95cd34cca292e5
Geisonoliveira <gs_gaara@hotmail.com>
2021-08-12 17:15:03 -0300

    Novo exercício

---
+++ Exercícios Propostos 5 - Classes/Exercicio2/Exercicio2/Pessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercicio2
+{
+    class Pessoa
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ Exercícios Propostos 5 - Classes/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa a, b;
+            a = new Pessoa();
+            b = new Pessoa();
+            
+            
+            Console.WriteLine("Digite um nome: ");
+            a.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite valor do salário: ");
+            a.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite um nome: ");
+            b.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite valor do salário: ");
+            b.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (a.salario + b.salario) / 2;
+
+            Console.WriteLine($"A média de salário é = {media.ToString("F3", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
