660e5cadc49865c365731013445628ead41ebf8b
Geison  Oliveira <gs_gaara@hotmail.com>
2021-08-14 17:38:36 -0300

    Nova aula

---
+++ 15 - SEÇÃO 4 - Classes, atributos, metodos/Criando Primeira Classe/Criando Primeira Classe/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Criando_Primeira_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X, Y;
+            X = new Triangulo();
+            Y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do Triangulo X: ");
+            X.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do Triangulo Y: ");
+            Y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = X.Area();
+
+            double areaY = Y.Area();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ 15 - SEÇÃO 4 - Classes, atributos, metodos/Criando Primeira Classe/Criando Primeira Classe/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Criando_Primeira_Classe
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
