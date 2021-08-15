ca064e1c578745e156b570bf1dcc16ccedea242d
Geison  Oliveira <gs_gaara@hotmail.com>
2021-08-15 20:39:40 -0300

    Primeiro Exercicio de Classes e Metodos

---
+++ Exercícios Propostos 5 - Classes/Exercicios propostos - Classes, Atributos e Metodos/PrimeiroExercicio/PrimeiroExercicio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace PrimeiroExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo R = new Retangulo();
+            
+            Console.WriteLine("Entre com a altura e largura do retangulo: ");
+            R.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            R.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Area = {R.CalcularArea().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Perimetro = {R.CalcularPerimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Diagonal = {R.CalcularDiagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Exercícios Propostos 5 - Classes/Exercicios propostos - Classes, Atributos e Metodos/PrimeiroExercicio/PrimeiroExercicio/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroExercicio
+{
+    class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double CalcularArea()
+        {
+            return Altura * Largura;
+        }
+
+        public double CalcularPerimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt((Altura * Altura) + (Largura * Largura));
+        }
+    }
+}
