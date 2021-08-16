0ab5bf4e4e5fab2f13aa29d3eded13c0772d6057
Geison  Oliveira <gs_gaara@hotmail.com>
2021-08-16 07:23:38 -0300

    Segundo Exercicio Classes e Metodos

---
+++ Exercícios Propostos 5 - Classes/Exercicios propostos - Classes, Atributos e Metodos/SegundoExercicio/SegundoExercicio/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace SegundoExercicio
+{
+    class Funcionario
+    {
+        public string Nome;
+
+        public double SalarioBruto;
+
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        //public override string ToString()
+        //{
+        //    return Nome
+        //        + ", $ "
+        //        + SalarioLiquido();
+        //}
+
+
+    }
+}
---
+++ Exercícios Propostos 5 - Classes/Exercicios propostos - Classes, Atributos e Metodos/SegundoExercicio/SegundoExercicio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace SegundoExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+            
+            Console.WriteLine("Entrar do dados de salario do empregado: ");
+            Console.WriteLine();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+
+            Console.Write("Salario Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(f.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
