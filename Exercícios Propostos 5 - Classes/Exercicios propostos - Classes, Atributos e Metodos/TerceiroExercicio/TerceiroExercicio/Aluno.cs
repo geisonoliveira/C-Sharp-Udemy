b8293d3c4169587bf3c5efeeffb9721076565f80
Geison  Oliveira <gs_gaara@hotmail.com>
2021-08-16 21:46:14 -0300

    Terceiro Exercicio Classes e Metodos

---
+++ Exercícios Propostos 5 - Classes/Exercicios propostos - Classes, Atributos e Metodos/TerceiroExercicio/TerceiroExercicio/Aluno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace TerceiroExercicio
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+        public string Situacao;
+        public double NotaRestante;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+       public void Aprovacao()
+        {
+            if (NotaFinal() >= 100)
+            {
+                Situacao = "APROVADO";
+            }
+
+            else
+            {
+                Situacao = "REPROVADO";
+            }
+        }
+
+        public void Restante()
+        {
+            if (Situacao == "REPROVADO")
+            {
+                NotaRestante = NotaFinal() - 100;
+            }
+
+        }
+
+    }
+}
---
+++ Exercícios Propostos 5 - Classes/Exercicios propostos - Classes, Atributos e Metodos/TerceiroExercicio/TerceiroExercicio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace TerceiroExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+            
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            a.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Nota Final: {a.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+            a.Aprovacao();
+
+            Console.WriteLine(a.Situacao);
+
+            a.Restante();
+            if (a.Situacao == "REPROVADO")
+            {
+                Console.WriteLine(a.NotaRestante.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            
+        }
+    }
+}
