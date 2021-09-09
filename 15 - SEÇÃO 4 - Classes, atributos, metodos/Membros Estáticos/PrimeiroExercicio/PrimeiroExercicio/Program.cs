e2ee6444e822e7e688d4b1937ffc60caa6976c44
Geison  Oliveira <gs_gaara@hotmail.com>
2021-09-09 07:02:47 -0300

    Membros Estaticos

---
+++ 15 - SEÇÃO 4 - Classes, atributos, metodos/Membros Estáticos/PrimeiroExercicio/PrimeiroExercicio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroExercicio
+{
+    class Program
+    {
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Entre com valor do raio: ");
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            double vol = Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+        static double Circunferencia (double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        static double Volume (double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
